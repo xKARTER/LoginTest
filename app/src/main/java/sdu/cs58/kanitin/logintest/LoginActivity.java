c9257a531791e637fed025f4cc718cacfcb4b901
KARTER <Kanitin21321@gamil.com>
2018-02-07 12:08:29 +0700

    Login Complete

--- app/src/main/java/sdu/cs58/kanitin/logintest/LoginActivity.java
+++ app/src/main/java/sdu/cs58/kanitin/logintest/LoginActivity.java
@@ -2,12 +2,53 @@ package sdu.cs58.kanitin.logintest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Explicit
+    EditText nameEditText,userNameEditText, passwordEditText;
+    Button loginButton;
+    String nameString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+
+
+        //Initial view ผูกตัวแปรบน JAVA ให้รู้จัก element บน xml
+        nameEditText = findViewById(R.id.txtName);
+        userNameEditText = findViewById(R.id.txtName);
+        passwordEditText = findViewById(R.id.txtName);
+        loginButton = findViewById(R.id.btnLogin);
+
+
+
+
+
+        loginButton .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameString = nameEditText.getText().toString().trim();
+
+
+
+
+                Toast.makeText(getApplicationContext(), "john cena "+ nameString , Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
+
+
+
+
+    } //END Void
+}//END
